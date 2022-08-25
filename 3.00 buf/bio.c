794cc56281b24b2a23b19258d78d5e399231165d
HunterPlus <zlw08140957@gmail.com>
2022-08-25 10:03:30 +0800

    Update bio.c

--- 3.00 buf/bio.c
+++ 3.00 buf/bio.c
@@ -43,7 +43,62 @@ void bhinit()
         (bp)->b_flags |= B_BUSY;                \
 }
 
-
+/*
+ * Assign a buffer for the given block.  If the appropriate
+ * block is already associated, return it; otherwise search
+ * for the oldest non-busy buffer and reassign it.
+ */
+struct buf *getblk(dev_t dev; daddr_t blkno)
+{
+        struct buf *bp, *dp, *ep;
+        int     dblkno = fsbtodb(dev, blkno);
+        
+        if ((unsigned)blkno >= 1 << (sizeof(int) * NBBY - PGSHIFT))
+                blkno = 1 << ((sizeof(int) *NBBY - PGSHIFT) + 1);
+        dblkno = fsbtodb(dev, blkno);
+        dp = BUFHASH(dev, dblkno);
+loop:
+        for (bp = dp->b_forw; bp != dp; bp = bp->b_forw) {
+                if (bp->b_blkno != dblkno || bp->b_dev != dev ||
+                                bp->b_flags & B_INVAL)
+                        continue;
+                if (bp->b_flags & B_BUSY) {
+                        bp->b_flags |= B_WANTED;
+                        sleep((caddr_t)bp, PRIBIO+1);
+                        goto loop;
+                }
+                notavail(bp);
+                bp->b_flags |= B_CACHE;
+                return (bp);
+        }
+        if (major(dev) >= nblkdev)      /* max block device ? */
+                panic("blkdev");        /* error message */
+        for (ep = &bfreelist[BQUEUES-1]; ep > bfreelist; ep--)
+                if (ep->av_forw != ep)
+                        break;
+        if (ep == bfreelist) {          /* no free blocks at all */
+                ep->b_flags |= B_WANTED;
+                sleep((caddr_t)ep, PRIBIO+1);
+                goto loop;
+        }
+        bp = ep->av_forw;
+        notavail(bp);
+        if (bp->b_flags & B_DELWRI) {
+                bp->b_flags |= B_ASYNC;
+                bwrite(bp);
+                goto loop;
+        }
+        bp->b_flags = B_BUSY;
+        bp->b_back->b_forw = bp->b_forw;
+        bp->b_forw->b_back = bp->b_back;
+        bp->b_forw = dp->b_forw;
+        bp->b_back = dp;
+        dp->b_forw->b_back = bp;
+        dp->b_forw = bp;
+        bp->b_dev = dev;
+        bp->b_blkno = dblkno;
+        return (bp);
+}
 
 /*
  * get an empty block,
