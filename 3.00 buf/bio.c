e4c92b770f19fee7d99d592d62901f0a6ebd1000
HunterPlus <zlw08140957@gmail.com>
2022-08-25 08:15:18 +0800

    Update bio.c

--- 3.00 buf/bio.c
+++ 3.00 buf/bio.c
@@ -12,3 +12,26 @@
  *	bawrite
  *	brelse
  */
+
+struct buf bfreelist[BQUEUES];
+struct buf bswlist, *bclnlist;
+
+#define BUFHSZ  63
+struct bufhd bufhash[BUFHSZ];
+#define BUFHASH(dev, dblkno) \
+                ((struct buf *) &bufhash[((int)(dev) + (int)(dblkno)) % BUFHSZ])
+/*
+ * Initialize hash links for buffers.
+ */
+void bhinit()
+{
+        int     i;
+        struct bufhd *bp;
+        
+        for (bp = bufhash, i = 0; i < BUFHSZ; i++, bp++)
+                bp->b_forw = bp->b_back = (struct buf *) bp;
+}
+
+
+
+
