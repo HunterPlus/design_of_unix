4ee98628d82452ad244ff7ba6f8cc55ecd677182
HunterPlus <zlw08140957@gmail.com>
2022-08-26 10:34:49 +0800

    Update iget.c

--- 4.00 inode/iget.c
+++ 4.00 inode/iget.c
@@ -26,3 +26,19 @@ void ihinit()
         for (i = 0; i < INOHSZ; i++)
                 inohash[i] = -1;
 }
+
+/*
+ * Find an inode if it is incore.
+ * This is the equivalent, for inodes,
+ * of ``incore'' in bio.c or ``pfind'' in subr.c.
+ */
+struct inode *ifind(dev_t dev, ino_t ino, int fstyp)
+{
+        struct inode *ip;
+        
+        for (ip = &inode[inohash[INOHASH(dev, ino, fstyp)]]; ip != &inode[-1]; ip = &inode[ip->i_hlink])
+                if (ino == ip->i_number && dev == ip->i_dev)
+                        return (ip);
+        return ((struct inode *)0);
+}
+
